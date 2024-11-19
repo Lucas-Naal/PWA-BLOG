14c53a3ca412c427f007527ff588386f64e71f87
Lucas Naal <lucasnaal8@gmail.com>
2024-11-18 20:27:24 -0600

    Second

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,43 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_NAME = "my-pwa-cache-v1";
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/bootstrap-5.3.3/css/bootstrap.min.css',
-    '/js/app.js',
-    '/bootstrap-5.3.3/js/bootstrap.min.js',
-    '/manifest.json',
+  "/",
+  "/index.html",
+  "/bootstrap-5.3.3/css/bootstrap.min.css",
+  "/js/app.js",
+  "/bootstrap-5.3.3/js/bootstrap.min.js",
+  "/manifest.json",
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Archivos en caché');
-                return cache.addAll(urlsToCache).catch((error) => {
-                    console.error('Error al agregar archivos a la caché:', error);
-                });
-            })
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Archivos en caché");
+      return cache.addAll(urlsToCache).catch((error) => {
+        console.error("Error al agregar archivos a la caché:", error);
+      });
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        console.log('Eliminando caché antigua:', cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log("Eliminando caché antigua:", cacheName);
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
